c8ead7961cd4af1a579e0b0663b0a0f7a4011e8f
kadirijhay <39244828+KadiriJhay@users.noreply.github.com>
2019-07-19 10:11:24 +0100

    Updates

---
+++ precache-manifest.d7d626e6b22d2a3df8bc316ec2ff8724.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36c33035939d2d83b87fda3951080c93",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b5c94a4c3a7d84870fdc",
+    "url": "./static/css/main.223ff0d7.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "./static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "b5c94a4c3a7d84870fdc",
+    "url": "./static/js/main.5cf80655.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
